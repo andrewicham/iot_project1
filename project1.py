aaae69125a6f7fabd837a3daf390d878612d522e
jacob-heebner <55461326+jacob-heebner@users.noreply.github.com>
2019-10-07 19:34:41 -0400

    Update project1.py
    step 2 done

--- project1.py
+++ project1.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+import threading
 
 PowerInputPin = 8
 LedPinOne = 24
-LedPinTwo = 12
+LedPinTwo = 16
 BtnOnePin = 18
-BtnTwoPin = 26
+BtnTwoPin = 22
 
 
 
@@ -15,39 +16,67 @@ def setup():
     GPIO.setup(LedPinTwo, GPIO.OUT)
 
     GPIO.setup(BtnOnePin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
+    GPIO.setup(BtnTwoPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(BtnOnePin, GPIO.RISING)
+    GPIO.add_event_detect(BtnTwoPin, GPIO.RISING)
     GPIO.output(LedPinOne, GPIO.HIGH)
 
 
-def loop():
+def lightOne():
     flashTime = 2
     counter = 0
-    buttonToggle = False
+    buttonToggle = True
+    buttonToggleTwo = True
     while True:
-        
-        GPIO.add_event_detect(channel, GPIO.input(BtnOnePin))
         GPIO.output(LedPinOne, GPIO.LOW)
         time.sleep(flashTime)
         GPIO.output(LedPinOne, GPIO.HIGH)
         time.sleep(flashTime)
-        #input_state = GPIO.input(BtnOnePin)
-        if GPIO.event_detect(channel):
-            buttonToggle = not buttonToggle
-            print('toggled')
+
+        if GPIO.event_detected(BtnTwoPin):
+			buttonToggleTwo = not buttonToggleTwo
+			print('button two toggled')
+		
+        if buttonToggleTwo == True:
+			if GPIO.event_detected(BtnOnePin):
+				buttonToggle = not buttonToggle
+				print('button one toggled')
+		
         if buttonToggle == True:
-            if counter != 5:
-                flashTime = flashTime / 1.5
+            if counter != 4:
+                flashTime = flashTime / 1.7
                 counter = counter + 1
-            elif counter == 5:
+            elif counter == 4:
                 counter = 0
                 flashTime = 2
                 
-                
-        else:
-            print('led off')
-            GPIO.output(LedPinOne, GPIO.LOW)
-            
+def lightTwo():
+	flashTime = 0.5
+	counter = 0
+	buttonToggle = True
+	buttonToggleTwo = False
+	while True:
+		GPIO.output(LedPinTwo, GPIO.LOW)
+		time.sleep(flashTime)
+		GPIO.output(LedPinTwo, GPIO.HIGH)
+		time.sleep(flashTime)
+		
+		if GPIO.event_detected(BtnTwoPin):
+			buttonToggleTwo = not buttonToggleTwo
+			print('button 2 toggled')
+		
+		if buttonToggleTwo == True:
+		    if GPIO.event_detected(BtnOnePin):
+			    buttonToggle = not buttonToggle
+			    print('button 1 toggled')
         
+		if buttonToggle == True:
+			if counter != 3:
+				flashTime = flashTime * 1.3
+				counter = counter + 1
+			elif counter == 3:
+				counter = 0
+				flashTime = 2
 
     
 
@@ -58,12 +87,11 @@ def destroy():
 if __name__ == '__main__':
     setup()
     try:
-        loop()
+		t1 = threading.Thread(target=lightOne)
+		t2 = threading.Thread(target=lightTwo)
+		t1.start()
+		t2.start()
+		t1.join()
+		t2.join()
     except KeyboardInterrupt:
         destroy()
-    
-
-
-
-
-
