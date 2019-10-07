8af45125f6ca8cb41657499b4da594e59492e51c
Andrew Chambers <35543146+andrewicham@users.noreply.github.com>
2019-10-07 17:47:14 -0400

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,22 +1,69 @@
 import RPi.GPIO as GPIO
+import time
 
 PowerInputPin = 8
-LedPinOne = 14
-LedPinTwo = 24
+LedPinOne = 24
+LedPinTwo = 12
 BtnOnePin = 18
-BtnTwoPin = 20
+BtnTwoPin = 26
+
+
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(LedPinOne, GPIO.OUT) #sets both Led Pins to output mode
     GPIO.setup(LedPinTwo, GPIO.OUT)
 
-    Gpio.setup(BtnOnePin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(BtnOnePin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    GPIO.output(LedPin, GPIO.HIGH)
+    GPIO.output(LedPinOne, GPIO.HIGH)
+
 
 def loop():
+    flashTime = 2
+    counter = 0
+    buttonToggle = False
     while True:
-        if GPIO.input(BtnOnePin) == GPIO.LOW:
-            print('led on')
-            GPIO.output(LedPin, GPIO.LOW)
+        
+        GPIO.add_event_detect(channel, GPIO.input(BtnOnePin))
+        GPIO.output(LedPinOne, GPIO.LOW)
+        time.sleep(flashTime)
+        GPIO.output(LedPinOne, GPIO.HIGH)
+        time.sleep(flashTime)
+        #input_state = GPIO.input(BtnOnePin)
+        if GPIO.event_detect(channel):
+            buttonToggle = not buttonToggle
+            print('toggled')
+        if buttonToggle == True:
+            if counter != 5:
+                flashTime = flashTime / 1.5
+                counter = counter + 1
+            elif counter == 5:
+                counter = 0
+                flashTime = 2
+                
+                
+        else:
+            print('led off')
+            GPIO.output(LedPinOne, GPIO.LOW)
+            
+        
+
+    
+
+def destroy():
+    GPIO.output(LedPinOne, GPIO.HIGH)
+    GPIO.cleanup()
+    
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+    
+
+
+
+
+
